c2e66d232d457393873a137a29524943eb62f5cf
AleksandrBK <lebedevavlv@gmail.com>
2023-07-20 12:51:39 +0400

    Task 36

--- task36/Program.cs
+++ task36/Program.cs
@@ -4,3 +4,24 @@
 // [-4, -6, 89, 6] -> 0
 
 
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+
+int[] array = GetArray(6, -100, 100);
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+
+
+int countOddPosition = 0;
+for (int i = 1; i < array.Length; i = i+2)
+{
+    countOddPosition += array[i];
+}
+
+Console.WriteLine($"Сумма чисел на нечетных позициях: {countOddPosition}");
