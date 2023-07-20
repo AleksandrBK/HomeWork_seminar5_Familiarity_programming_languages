88705672c0b1929b1e385101335924ad3c6fedd9
AleksandrBK <lebedevavlv@gmail.com>
2023-07-20 12:36:50 +0400

    Task 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -4,3 +4,28 @@
 // [345, 897, 568, 234] -> 2
 
 
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+int[] array = GetArray(4, 100, 1000);
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+
+
+int countEven = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] % 2;
+    if (array[i] == 0)
+    {
+        countEven++;
+    }
+}
+
+Console.WriteLine($"Количество четных чисел: {countEven}");
